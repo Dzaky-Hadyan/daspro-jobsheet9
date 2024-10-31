be0f3171505b497397c8cd8440ac4c4d0669e00a
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2024-10-31 10:40:14 +0700

    feat: adding tugas 3

---
+++ Tugas3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Tugas3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"};
+        System.out.print("Masukkan nama makanan yang ingin dicari: ");
+        String makanan = sc.nextLine();
+        boolean ada = false;
+        for (int i = 0; i < menu.length; i++) {
+            if (makanan.equalsIgnoreCase(menu[i])) {
+                ada = true;
+            } else {
+                ada = false;
+            }
+        }
+        if (ada) {
+            System.out.print(makanan + " tersedia");
+        } else {
+            System.out.print(makanan + " tidak tersedia");
+        }
+        sc.close();
+    }
+}
